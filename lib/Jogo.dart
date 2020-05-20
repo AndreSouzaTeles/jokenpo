3f5c5c3ee8c47201669c5714a023d8188c07807a
Andre Souza Teles <andre.souza.teles@hotmail.com>
2020-05-19 23:52:05 -0300

    Game jokenPo

---
+++ lib/Jogo.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  var _imagemApp = AssetImage("images/padrao.png");
+  var _mensagem = "Sua vez!!! Escolha uma opção abaixo: ";
+  var _mensagemResultado = "SERA QUE CONSEGUE GANHAR??";
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+
+    print("Opção selecionada " + escolhaApp);
+    print("Escolha do usuario: " + escolhaUsuario);
+
+    switch (escolhaApp) {
+      case "pedra":
+        setState(() {
+          this._imagemApp = AssetImage("images/pedra.png");
+        });
+        break;
+      case "papel":
+        setState(() {
+          this._imagemApp = AssetImage("images/papel.png");
+        });
+        break;
+      case "tesoura":
+        setState(() {
+          this._imagemApp = AssetImage("images/tesoura.png");
+        });
+        break;
+    }
+
+    //Validação
+    //usuario ganhou
+    if ((escolhaUsuario == "pedra" && escolhaApp == "tesoura") ||
+        (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
+        (escolhaUsuario == "tesoura" && escolhaApp == "papel")) {
+      setState(() {
+        this._mensagemResultado = "Parabéns!!! Você ganhou";
+      });
+    }
+    //App ganhador
+    else if ((escolhaApp == "pedra" && escolhaUsuario == "tesoura") ||
+        (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+        (escolhaApp == "tesoura" && escolhaUsuario == "papel")) {
+      setState(() {
+        this._mensagemResultado = "Sorry!!! Você perdeu";
+      });
+
+    }else{
+      setState(() {
+        this._mensagemResultado = "WOW, Empatamos!";
+      });
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("jokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          //1)text
+          //2)imagem
+          //3)text resultado
+          //4)Linha de 3 imagens
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Minha Escolha: ",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Image(image: this._imagemApp),
+          Padding(
+            padding: EdgeInsets.only(top: 60, bottom: 16),
+            child: Text(
+              this._mensagem,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset(
+                  "images/pedra.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset(
+                  "images/papel.png",
+                  height: 100,
+                ),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset(
+                  "images/tesoura.png",
+                  height: 100,
+                ),
+              ),
+              /*
+              Image.asset("images/pedra.png", height: 100,),
+              Image.asset("images/papel.png",height: 100,),
+              Image.asset("images/tesoura.png", height: 100,),
+              */
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 15.0, top: 80.0, right: 15.0, bottom: 15.0),
+                child: Text(
+                  _mensagemResultado,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+      bottomSheet: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              "Tente me derrotar!!!",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
